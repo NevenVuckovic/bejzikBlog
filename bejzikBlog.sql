fb89e18f3380da46a98a43760259f32a86ee047f
Neven Vuckovic <nvn.vckvc@gmail.com>
2020-10-14 20:44:21 +0200

    zavrsen Zadatak 4

---
+++ bejzikBlog.sql
@@ -0,0 +1,13 @@
+-- create table posts (
+--     id int primary key auto_increment,
+--     title varchar(30) not null,
+--     body varchar(1000) not null,
+--     author varchar(20) not null,
+--     created_at DATETIME NOT NULL DEFAULT NOW()
+-- )
+
+-- insert into posts (title, body, author) values 
+--     ('Prvi Naslov', 'loremipsum', 'Lj. Ipsumovic'),
+--     ('Drugi Naslov', 'loremipsumloremispum', 'Ljore I.'),
+--     ('treci Naslov', 'loremipsumloremispumloremipsum', 'Lj.I.')
+
---
+++ posts.php
@@ -0,0 +1,24 @@
+<?php
+$sql1 = "SELECT * FROM posts ORDER BY created_at DESC";
+$statement = $connection->prepare($sql1);
+$statement->execute();
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+$posts = $statement->fetchAll();
+?>
+
+<?php
+foreach ($posts as $post) {
+?>
+<article class="blog-post">
+    <header>
+        <h1 class="blog-post-title"><a class="naslov" href="#"<?php echo($post['id']) ?>><?php echo($post['title']) ?></a></h1>
+        <div> <?php echo($post['created_at']) ?> by <?php echo($post['author']) ?></div>
+    </header>
+        <div>
+        <p> <?php echo($post['body']) ?></p>
+        </div>
+</article>
+
+<?php
+ }
+?>
